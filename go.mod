41417b9960b3492023ec63ad5d3fb6780d4a1991
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-18 03:05:43 +0700

    add logging middleware

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.17
 require github.com/labstack/gommon v0.3.1
 
 require (
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+)
+
+require (
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	config "github.com/furqonzt99/refresh-token/configs"
+	"github.com/furqonzt99/refresh-token/delivery/middlewares"
 	"github.com/furqonzt99/refresh-token/utils"
 	"github.com/labstack/echo/v4"
 )
 
-func main()  {
+func main() {
 	config := config.GetConfig()
 
 	db := utils.InitDB(config)
 
 	utils.InitialMigrate(db)
-	
+
 	e := echo.New()
+
+	// logging
+	middlewares.LogMiddleware(e)
 
 	e.Logger.Fatal(e.Start(":" + config.Port))
 }
