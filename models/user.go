2b2bf854b8d0e79fbadfc4d79d25f3ab30582780
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-18 02:38:27 +0700

    add user model

---
+++ models/user.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role" gorm:"default:user"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) BeforeCreate(db *gorm.DB) error {
+	u.ID = uuid.New().String()
+	u.CreatedAt = time.Now().Local()
+	return nil
+}
+
+func (u *User) BeforeUpdate(db *gorm.DB) error {
+	u.UpdatedAt = time.Now().Local()
+	return nil
+}
