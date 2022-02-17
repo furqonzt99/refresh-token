cfb1a45b79d9a7af534351be589238b799a3d8b6
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-18 04:00:46 +0700

    add global validator

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	config "github.com/furqonzt99/refresh-token/configs"
+	"github.com/furqonzt99/refresh-token/delivery/common"
 	"github.com/furqonzt99/refresh-token/delivery/middlewares"
 	"github.com/furqonzt99/refresh-token/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -22,6 +24,9 @@ func main() {
 
 	// remove trailing slash
 	e.Pre(middleware.RemoveTrailingSlash())
+
+	// validator
+	e.Validator = &common.Validator{Validator: validator.New()}
 
 	e.Logger.Fatal(e.Start(":" + config.Port))
 }
