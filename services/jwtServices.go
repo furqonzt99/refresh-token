ee9164a3f1843106637a68a77649b5223bca3c8a
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-18 16:43:58 +0700

    update jwt services validation

--- services/jwtServices.go
+++ services/jwtServices.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/furqonzt99/refresh-token/constants"
@@ -16,13 +17,14 @@ func CreateAccessToken(userId string, email, role string) (string, error) {
 	claims["userId"] = userId
 	claims["email"] = email
 	claims["role"] = role
-	claims["exp"] = time.Now().Add(time.Minute * 5).Unix()
+	claims["exp"] = time.Now().Add(time.Minute * 1).Unix() // 1 minute expired
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(constants.JWT_ACCESS_KEY))
 }
 
 func ExtractAccessToken(e echo.Context) (models.Payload, error) {
 	user := e.Get("user").(*jwt.Token)
+	fmt.Println(user)
 	if user.Valid {
 		claims := user.Claims.(jwt.MapClaims)
 		userId := claims["userId"]
@@ -41,18 +43,36 @@ func CreateRefreshToken(accessToken string) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["accessToken"] = accessToken
-	claims["exp"] = time.Now().Add(time.Hour * 24 * 60).Unix()
+	claims["exp"] = time.Now().Add(time.Hour * 24 * 60).Unix() // 60 days expired
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(constants.JWT_ACCESS_KEY))
+	return token.SignedString([]byte(constants.JWT_REFRESH_KEY))
 }
 
-func ValidateRefreshToken(e echo.Context) (string, error) {
-	user := e.Get("user").(*jwt.Token)
-	if user.Valid {
-		claims := user.Claims.(jwt.MapClaims)
-		tokenId := claims["tokenId"]
-		return tokenId.(string), nil
+func ValidateRefreshToken(refreshToken string) (models.Payload, error) {
+
+	// get accessToken from Refresh Token
+	oldRefreshToken, _ := jwt.Parse(refreshToken, func(oldRefreshToken *jwt.Token) (interface{}, error) {
+		return []byte(constants.JWT_REFRESH_KEY), nil
+	})
+
+	// get Payload from Old Access Token
+	accToken := oldRefreshToken.Claims.(jwt.MapClaims)["accessToken"].(string)
+	oldAccessToken, _ := jwt.Parse(accToken, func(oldAccessToken *jwt.Token) (interface{}, error) {
+		return []byte(constants.JWT_ACCESS_KEY), nil
+	})
+
+	claims := oldAccessToken.Claims.(jwt.MapClaims)
+
+	if claims["email"].(string) != "" {
+		userId := claims["userId"]
+		email := claims["email"]
+		role := claims["role"]
+		return models.Payload{
+			UserID: userId.(string),
+			Email:  email.(string),
+			Role:   role.(string),
+		}, nil
 	}
 
-	return "", errors.New("invalid token")
+	return models.Payload{}, errors.New("invalid token")
 }
