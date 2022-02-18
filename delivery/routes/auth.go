78a0186ea08e4b3c074f2f3b22d43eb8091e64f6
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-02-18 16:42:51 +0700

    add refresh token routes

--- delivery/routes/auth.go
+++ delivery/routes/auth.go
@@ -8,4 +8,5 @@ import (
 func RegisterAuthPath(e *echo.Echo, authController *auth.AuthController) {
 	e.POST("/auth/register", authController.Register)
 	e.POST("/auth/login", authController.Login)
+	e.POST("/auth/refresh", authController.Refresh)
 }
